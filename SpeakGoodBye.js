ab09cadf148d32329fd11e83a9c97d181e52dced
vedashree18 <66110157+vedashree18@users.noreply.github.com>
2020-06-23 11:20:14 +0530

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,5 +1,9 @@
-var speakWord = "Good Bye";
-function speak(name) {
-  console.log(speakWord + " " + name);
+(function(window){
+	var byeSpeaker={};
+    var speakWord = "Good Bye";
+     byeSpeaker.speak=function(name) {
+          console.log(speakWord + " " + name);
 }
 
+window.byeSpeaker=byeSpeaker;
+})(window);
